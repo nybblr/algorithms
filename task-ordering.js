634f0336e260787ad9d570f5f0aac69cbb245c05
Jonathan Martin <me@nybblr.com>
2018-09-24 10:31:33 -0400

    Add multiple task ordering solutions.

--- task-ordering.js
+++ task-ordering.js
@@ -1,4 +1,53 @@
-const assert = require('assert');
+let tasks = {
+  'chores': ['mop', 'interview', 'sweep'],
+  'mop': ['sweep', 'dinner'],
+  'interview': ['algfriyay', 'dinner'],
+  'sweep': [],
+  'algfriyay': [],
+  'dinner': [],
+};
+
+// Imperative solution
+let order = (tasks) => {
+  let done = new Set();
+  let taskNames = Object.keys(tasks);
+  let taskCount = taskNames.length;
+
+  for (let i = 0; i < taskCount; i++) {
+    let taskToDo = taskNames.find(task =>
+      tasks[task].every(subtask =>
+        done.has(subtask)
+      )
+    );
+    done.add(taskToDo);
+    taskNames = taskNames.filter(task => task !== taskToDo);
+  }
+
+  return done;
+};
+
+// Recursive solution, unoptimized
+let order = (tasks, start) => {
+  let done = new Set();
+  let doer = task => {
+    tasks[task].forEach(subtask => doer(subtask));
+    done.add(task);
+  };
+  doer(start);
+  return [...done];
+};
+
+// Recursive solution, optimized
+// A.K.A. "Dynamic programming"
+let order = (tasks, start) => {
+  let done = new Set();
+  let doer = memoize(t => t, task => {
+    tasks[task].forEach(subtask => doer(subtask));
+    done.add(task);
+  });
+  doer(start);
+  return [...done];
+};
 
 // Dynamic programming utility
 let memoize = (keyGenerator, op) => {
@@ -13,26 +62,8 @@ let memoize = (keyGenerator, op) => {
   };
 };
 
-let order = (tasks, start) => {
-  let ordered = new Set();
-  let explore = memoize(n => n, root => {
-    tasks[root].forEach(task => explore(task));
-    ordered.add(root);
-  });
-  explore(start);
-  ordered.add(start);
-  return [...ordered];
-};
-
-let tasks = {
-  'chores': ['mop', 'interview', 'sweep'],
-  'mop': ['sweep', 'dinner'],
-  'interview': ['algfriyay', 'dinner'],
-  'sweep': [],
-  'algfriyay': [],
-  'dinner': [],
-};
-
+// Tests
+const assert = require('assert');
 assert.deepEqual(
   order(tasks, 'chores'), [
     'sweep',
